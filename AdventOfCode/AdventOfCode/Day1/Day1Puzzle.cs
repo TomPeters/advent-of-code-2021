7985561ca99231fb2ae83555e5c9b947cb83d8de
Tom Peters <tom.peters@octopus.com>
2022-02-16 21:41:13 +1000

    Solution to Day1 part 2

--- AdventOfCode/AdventOfCode/Day1/Day1Puzzle.cs
+++ AdventOfCode/AdventOfCode/Day1/Day1Puzzle.cs
@@ -4,6 +4,38 @@ public static class Day1Puzzle
 {
     public static int GetNumberOfMeasurementsLargerThatPreviousMeasurement(int[] measurements)
     {
-        return measurements.Skip(1).Zip(measurements).Count(m => m.First > m.Second);
+        return measurements.Skip(1)
+            .Zip(measurements)
+            .Count(m => m.First > m.Second);
+    }
+
+    public static int GetNumberOfThreeMeasurementSlidingWindowsLargerThanPreviousWindow(int[] measurements)
+    {
+        var threeMeasurementWindows = GetThreeMeasurementWindows(measurements).ToArray();
+        return threeMeasurementWindows.Skip(1)
+            .Zip(threeMeasurementWindows)
+            .Count(m => m.First.Sum() > m.Second.Sum());
+    }
+
+    static IEnumerable<ThreeMeasurementWindow> GetThreeMeasurementWindows(int[] measurements)
+    {
+        return measurements.Skip(2)
+            .Zip(measurements.Skip(1), measurements)
+            .Select(m => new ThreeMeasurementWindow(new[] { m.First, m.Second, m.Third }));
+    }
+}
+
+class ThreeMeasurementWindow
+{
+    readonly int[] _values;
+
+    public ThreeMeasurementWindow(int[] values)
+    {
+        _values = values;
+    }
+
+    public int Sum()
+    {
+        return _values.Sum();
     }
 }
--- AdventOfCode/AdventOfCodeTests/Day1/Day1.cs
+++ AdventOfCode/AdventOfCodeTests/Day1/Day1.cs
@@ -8,30 +8,53 @@ namespace AdventOfCodeTests.Day1;
 public class Day1
 {
     [Fact]
-    public void WorksForSampleData()
+    public void Part1WorksForSampleData()
     {
-        var sampleMeasurements = new int[]
-        {
-            199,
-            200,
-            208,
-            210,
-            200,
-            207,
-            240,
-            269,
-            260,
-            263
-        };
-        Assert.Equal(7, Day1Puzzle.GetNumberOfMeasurementsLargerThatPreviousMeasurement(sampleMeasurements));
+        Assert.Equal(7, Day1Puzzle.GetNumberOfMeasurementsLargerThatPreviousMeasurement(SampleMeasurements));
     }
 
     [Fact]
-    public void SolvePuzzle()
+    public void Part2WorksForSampleData()
+    {
+        Assert.Equal(5, Day1Puzzle.GetNumberOfThreeMeasurementSlidingWindowsLargerThanPreviousWindow(SampleMeasurements));
+    }
+
+    [Fact]
+    public void SolvePart1()
     {
         var data = FileHelper.ReadFromFile("Day1", "Measurements.txt");
         var measurements = data.Split("\n").Select(int.Parse).ToArray();
 
         Assert.Equal(1400, Day1Puzzle.GetNumberOfMeasurementsLargerThatPreviousMeasurement(measurements));
+    }
+
+    [Fact]
+    public void SolvePart2()
+    {
+        var data = FileHelper.ReadFromFile("Day1", "Measurements.txt");
+        var measurements = data.Split("\n").Select(int.Parse).ToArray();
+
+        Assert.Equal(1429, Day1Puzzle.GetNumberOfThreeMeasurementSlidingWindowsLargerThanPreviousWindow(measurements));
+    }
+
+    static int[] SampleMeasurements
+    {
+        get
+        {
+            var sampleMeasurements = new int[]
+            {
+                199,
+                200,
+                208,
+                210,
+                200,
+                207,
+                240,
+                269,
+                260,
+                263
+            };
+            return sampleMeasurements;
+        }
     }
 }
