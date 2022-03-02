7bc7960d94c4d021fc1366ea857acf08d28b5e16
Tom Peters <tom.peters@octopus.com>
2022-03-03 08:44:03 +1000

    Cleanup

--- AdventOfCode/AdventOfCode/Day3/Day3Puzzle.cs
+++ AdventOfCode/AdventOfCode/Day3/Day3Puzzle.cs
@@ -13,74 +13,82 @@ public static class Day3Puzzle
         return gammaRate.ConvertToInt() * epsilonRate.ConvertToInt();
     }
 
+    static BinaryNumber GetPowerConsumptionFactor(IReadOnlyList<BinaryNumber> binaryNumbers, IBitCriteria bitCriteria)
+    {
+        var bits = Enumerable.Range(0, GetLengthOfEachBinaryNumber(binaryNumbers))
+            .Select(index => GetBitAtIndex(index, binaryNumbers, bitCriteria));
+
+        return new BinaryNumber(bits);
+    }
+
+    static bool GetBitAtIndex(int index, IReadOnlyList<BinaryNumber> binaryNumbers, IBitCriteria bitCriteria)
+    {
+        var bitsAtIndexAcrossAllNumbers = binaryNumbers.Select(n => n.GetBitAtIndex(index));
+        var bitGroups = bitsAtIndexAcrossAllNumbers.GroupBy(n => n, (c, bits) => new BitGroups(c, bits.Count()));
+        var bitSatisfyingBitCriteria = bitCriteria.GetBit(bitGroups);
+        if (bitSatisfyingBitCriteria is { } result)
+        {
+            return result;
+        }
+        throw new Exception($"Binary numbers did not have any values at index {index}");
+    }
+
     static int GetLengthOfEachBinaryNumber(IEnumerable<BinaryNumber> binaryNumbers)
     {
         var firstBinaryNumber = binaryNumbers.FirstOrDefault();
         return firstBinaryNumber?.GetLength() ?? 0;
     }
-
-    static BinaryNumber GetPowerConsumptionFactor(IReadOnlyList<BinaryNumber> binaryNumbers, IBitCriteria bitCriteria)
-    {
-        var factorChars = Enumerable.Range(0, GetLengthOfEachBinaryNumber(binaryNumbers)).Select(index =>
-        {
-            var allDigitsAtIndex = binaryNumbers.Select(n => n.GetValueAtIndex(index));
-            var digitGroups = allDigitsAtIndex.GroupBy(n => n, (c, chars) => new DigitGroups(c, chars.Count()));
-            var digitSatisfyingBitCriteria = bitCriteria.GetDigit(digitGroups);
-            if (digitSatisfyingBitCriteria is { } bitCriteriaDigit)
-            {
-                return bitCriteriaDigit;
-            }
-            throw new Exception($"Binary numbers did not have any values at index {index}");
-        }).ToArray();
-
-        return new BinaryNumber(new string(factorChars));
-    }
 }
 
 interface IBitCriteria
 {
-    char? GetDigit(IEnumerable<DigitGroups> digitGroups);
+    bool? GetBit(IEnumerable<BitGroups> bitGroups);
 }
 
 class GammaRateBitCriteria : IBitCriteria
 {
-    public char? GetDigit(IEnumerable<DigitGroups> digitGroups)
+    public bool? GetBit(IEnumerable<BitGroups> bitGroups)
     {
-        return digitGroups.MaxBy(group => group.Frequency)?.Digit;
+        return bitGroups.MaxBy(group => group.Frequency)?.Bit;
     }
 }
 
 class EpsilonRateBitCriteria : IBitCriteria
 {
-    public char? GetDigit(IEnumerable<DigitGroups> digitGroups)
+    public bool? GetBit(IEnumerable<BitGroups> bitGroups)
     {
-        return digitGroups.MinBy(group => group.Frequency)?.Digit;
+        return bitGroups.MinBy(group => group.Frequency)?.Bit;
     }
 }
 
-record DigitGroups(char Digit, int Frequency);
+record BitGroups(bool Bit, int Frequency);
 
 class BinaryNumber
 {
-    readonly char[] _chars;
+    readonly bool[] _bits;
 
     public BinaryNumber(string number)
     {
-        _chars = number.Select(c => c).ToArray();
+        _bits = number.Select(c => c != '0').ToArray();
     }
 
-    public char GetValueAtIndex(int index)
+    public BinaryNumber(IEnumerable<bool> number)
     {
-        return _chars[index];
+        _bits = number.ToArray();
+    }
+
+    public bool GetBitAtIndex(int index)
+    {
+        return _bits[index];
     }
 
     public int GetLength()
     {
-        return _chars.Count();
+        return _bits.Length;
     }
 
     public int ConvertToInt()
     {
-        return Convert.ToInt32(new string(_chars), 2);
+        return Convert.ToInt32(new string(_bits.Select(c => c ? '1' : '0').ToArray()), 2);
     }
 }
