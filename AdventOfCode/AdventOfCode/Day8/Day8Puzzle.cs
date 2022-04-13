ab9070d8dc4db452b4c637dc628e31102814b546
Tom Peters <tom.peters@octopus.com>
2022-04-13 19:52:53 +1000

    String parsing

--- AdventOfCode/AdventOfCode/Day8/Day8Puzzle.cs
+++ AdventOfCode/AdventOfCode/Day8/Day8Puzzle.cs
@@ -2,8 +2,41 @@ namespace AdventOfCode.Day8;
 
 public static class Day8Puzzle
 {
-    public static int SolvePuzzle(string input)
+    public static int SolvePuzzle(IEnumerable<Entry> entries)
     {
         return 0;
     }
+}
+
+public class SignalPattern
+{
+    public SignalPattern(string signals)
+    {
+    }
+}
+
+public class Entry
+{
+    public Entry(IEnumerable<SignalPattern> uniqueSignalPatterns, FourDigitOutputValue fourDigitOutputValue)
+    {
+        UniqueSignalPatterns = uniqueSignalPatterns.ToList();
+        FourDigitOutputValue = fourDigitOutputValue;
+    }
+
+    public List<SignalPattern> UniqueSignalPatterns { get; }
+    public FourDigitOutputValue FourDigitOutputValue { get; }
+}
+
+public class FourDigitOutputValue
+{
+    public FourDigitOutputValue(IEnumerable<SignalPattern> fourSignalPatterns)
+    {
+        FourSignalPatterns = fourSignalPatterns.ToList();
+        if (FourSignalPatterns.Count() != 4)
+        {
+            throw new ArgumentException("Number of signal patterns must be exactly 4", nameof(fourSignalPatterns));
+        }
+    }
+
+    public IEnumerable<SignalPattern> FourSignalPatterns { get; }
 }
--- AdventOfCode/AdventOfCodeTests/Day8/Day8.cs
+++ AdventOfCode/AdventOfCodeTests/Day8/Day8.cs
@@ -10,7 +10,7 @@ public class Day8
     [Fact]
     public void Part1WorksForSampleData()
     {
-        Assert.Equal(1, Day8Puzzle.SolvePuzzle(SampleData));
+        Assert.Equal(26, Day8Puzzle.SolvePuzzle(SampleData));
     }
 
     [Fact]
@@ -19,9 +19,9 @@ public class Day8
         Assert.Equal(1, Day8Puzzle.SolvePuzzle(RealData));
     }
 
-    static string RealData => FileHelper.ReadFromFile("Day8", "SignalPatterns.txt");
+    static IEnumerable<Entry> RealData => GetEntries(FileHelper.ReadFromFile("Day8", "SignalPatterns.txt"));
 
-    static string SampleData => @"be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+    static IEnumerable<Entry> SampleData => GetEntries(@"be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
 edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
 fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
 fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
@@ -30,5 +30,21 @@ fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fad
 dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
 bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
 egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
-gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce";
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce");
+
+    static IEnumerable<Entry> GetEntries(string input)
+    {
+        return input.Split("\n").Select(entryString =>
+        {
+            var parts = entryString.Split("|");
+            var uniqueSignalPatterns = ParseSignalPatternsFromSection(parts[0]);
+            var fourSignalOutputValue = ParseSignalPatternsFromSection(parts[1]);
+            return new Entry(uniqueSignalPatterns, new FourDigitOutputValue(fourSignalOutputValue));
+        });
+    }
+
+    static IEnumerable<SignalPattern> ParseSignalPatternsFromSection(string input)
+    {
+        return input.Split(" ").Select(signalPatternString => new SignalPattern(signalPatternString.Trim()));
+    }
 }
