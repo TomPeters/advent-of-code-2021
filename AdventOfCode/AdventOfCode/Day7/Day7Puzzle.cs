c5d6ff181ca2e5bb399078b1ccc0d09a783ec4e6
Tom Peters <tom.peters@octopus.com>
2022-04-13 13:16:02 +1000

    Working solution to day 7

--- AdventOfCode/AdventOfCode/Day7/Day7Puzzle.cs
+++ AdventOfCode/AdventOfCode/Day7/Day7Puzzle.cs
@@ -2,8 +2,68 @@ namespace AdventOfCode.Day7;
 
 public static class Day7Puzzle
 {
-    public static int GetFuelToAlignToPosition()
+    public static int GetFuelToAlignToPosition(int[] positions, IFuelCostCalculator fuelCostCalculator)
     {
-        return 0;
+        var positionRange = GetPositionRange(positions);
+
+        return Enumerable.Range(positionRange.MinPosition, positionRange.Range)
+            .Select(p => GetCostOfAlignment(p, positions, fuelCostCalculator))
+            .Min();
+    }
+
+    static int GetCostOfAlignment(int positionToAlignTo, int[] positions, IFuelCostCalculator fuelCostCalculator)
+    {
+        return positions.Sum(p =>
+        {
+            var distance = Math.Abs(p - positionToAlignTo);
+            return fuelCostCalculator.GetCostOfFuel(distance);
+        });
+    }
+
+    static PositionRange GetPositionRange(int[] positions)
+    {
+        var maxPosition = positions.Max();
+        var minPosition = positions.Min();
+        return new PositionRange(maxPosition, minPosition);
+    }
+}
+
+public interface IFuelCostCalculator
+{
+    int GetCostOfFuel(int distanceToMove);
+}
+
+public class ConstantFuelCostCalculator : IFuelCostCalculator
+{
+    public int GetCostOfFuel(int distanceToMove)
+    {
+        return distanceToMove;
+    }
+}
+
+public class IncrementingFuelCostCalculator : IFuelCostCalculator
+{
+    public int GetCostOfFuel(int distanceToMove)
+    {
+        if (distanceToMove % 2 == 0)
+        {
+            return GetCostOfFuel(distanceToMove + 1) - (distanceToMove + 1);
+        }
+
+        var oneValueHigher = distanceToMove + 1;
+        return (oneValueHigher * oneValueHigher / 2) - (oneValueHigher / 2);
+    }
+}
+
+class PositionRange
+{
+    public int MaxPosition { get; }
+    public int MinPosition { get; }
+    public int Range => MaxPosition - MinPosition;
+
+    public PositionRange(int maxPosition, int minPosition)
+    {
+        this.MaxPosition = maxPosition;
+        this.MinPosition = minPosition;
     }
 }
--- AdventOfCode/AdventOfCodeTests/Day7/Day7.cs
+++ AdventOfCode/AdventOfCodeTests/Day7/Day7.cs
@@ -10,14 +10,27 @@ public class Day7
     [Fact]
     public void Part1WorksForSampleData()
     {
-        Assert.Equal(1, Day7Puzzle.GetFuelToAlignToPosition());
+        Assert.Equal(37, Day7Puzzle.GetFuelToAlignToPosition(SampleData.ToArray(), new ConstantFuelCostCalculator()));
     }
 
     [Fact]
     public void Part1WorksForRealData()
     {
-        Assert.Equal(1, Day7Puzzle.GetFuelToAlignToPosition());
+        Assert.Equal(328187, Day7Puzzle.GetFuelToAlignToPosition(RealData.ToArray(), new ConstantFuelCostCalculator()));
     }
+
+    [Fact]
+    public void Part2WorksForSampleData()
+    {
+        Assert.Equal(168, Day7Puzzle.GetFuelToAlignToPosition(SampleData.ToArray(), new IncrementingFuelCostCalculator()));
+    }
+
+    [Fact]
+    public void Part2WorksForRealData()
+    {
+        Assert.Equal(91257582, Day7Puzzle.GetFuelToAlignToPosition(RealData.ToArray(), new IncrementingFuelCostCalculator()));
+    }
+
 
     static IEnumerable<int> RealData => FileHelper.ReadFromFile("Day7", "HorizontalPositions.txt").Split(",").Select(int.Parse);
 
