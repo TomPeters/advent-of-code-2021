eb8da87258b4da31881f9b7e19291f94345551b0
Tom Peters <tom.peters@octopus.com>
2022-04-13 19:37:46 +1000

    Boilerplate for day 8

---
+++ AdventOfCode/AdventOfCode/Day8/Day8Puzzle.cs
@@ -0,0 +1,9 @@
+namespace AdventOfCode.Day8;
+
+public static class Day8Puzzle
+{
+    public static int SolvePuzzle(string input)
+    {
+        return 0;
+    }
+}
---
+++ AdventOfCode/AdventOfCodeTests/Day8/Day8.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Day8;
+using Xunit;
+
+namespace AdventOfCodeTests.Day8;
+
+public class Day8
+{
+    [Fact]
+    public void Part1WorksForSampleData()
+    {
+        Assert.Equal(1, Day8Puzzle.SolvePuzzle(SampleData));
+    }
+
+    [Fact]
+    public void Part1WorksForRealData()
+    {
+        Assert.Equal(1, Day8Puzzle.SolvePuzzle(RealData));
+    }
+
+    static string RealData => FileHelper.ReadFromFile("Day8", "SignalPatterns.txt");
+
+    static string SampleData => @"be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce";
+}
